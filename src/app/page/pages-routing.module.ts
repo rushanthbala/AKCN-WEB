61e680473c27898e76fda4bc682958b836c19559
rushanthbala <rushanth9999@gmail.com>
2022-12-01 08:35:50 +0530

    align folder

--- src/app/page/pages-routing.module.ts
+++ src/app/page/pages-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminsComponent } from './admins/admins.component';
-import { CreateTicketComponent } from './create-ticket/create-ticket.component';
+import { CreateTicketComponent } from './ticket/create-ticket/create-ticket.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { EditTicketComponent } from './edit-ticket/edit-ticket.component';
+import { EditTicketComponent } from './ticket/create-ticket/edit-ticket/edit-ticket.component';
 import { PagesComponent } from './pages.component';
 import { PendingOneTicketComponent } from './pending-one-ticket/pending-one-ticket.component';
-import { PendingTicketComponent } from './pending-ticket/pending-ticket.component';
+import { PendingTicketComponent } from './ticket/pending-ticket/pending-ticket.component';
 
 const routes: Routes = [
   {
